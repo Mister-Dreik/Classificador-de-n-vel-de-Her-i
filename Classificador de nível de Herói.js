77043bcd1954b2fa4d1549c06d293f3ce0a42114
Mister-Dreik <135073041+Mister-Dreik@users.noreply.github.com>
2023-12-07 22:06:03 -0300

    Add files via upload

---
+++ Classificador de nível de Herói.js
@@ -0,0 +1,40 @@
+
+let nomeDoHeroi2 = "Shao Kahn"
+let xpHeroi2 = 5000
+let nivelHeroi2
+xpHeroi2 = 5000 * 2
+if (xpHeroi2 <= 1000){
+    nivelHeroi2 = "Ferro"
+    } else if (xpHeroi2 <= 2000){ 
+       nivelHeroi2 = "Bronze"}
+       else if (xpHeroi2 <= 5000){ 
+           nivelHeroi2 = "Prata"}
+           else if (xpHeroi2 <= 10001){ 
+            nivelHeroi2 = "radiante"}
+
+
+
+console.log("O Herói de nome " + nomeDoHeroi2 + " está no nível de " + nivelHeroi2   )
+
+
+
+let nomeDoHeroi1 = "Raiden"
+let xpHeroi = 10001
+let nivelHeroi 
+
+ if (xpHeroi <= 7000){
+ nivelHeroi = "ouro"
+ } else if (xpHeroi <= 9000){ 
+    nivelHeroi = "platina"}
+    else if (xpHeroi <= 10000){ 
+        nivelHeroi = "imortal"}
+        else if (xpHeroi <= 10001){ 
+            nivelHeroi = "radiante"    
+ }
+
+console.log("O Herói de nome " + nomeDoHeroi1 + " está no nível de " + nivelHeroi   )
+
+
+
+
+
